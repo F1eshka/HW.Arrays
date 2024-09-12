886068fdfd2e0e50e9c3e388dcafeab061db0875
F1eshka <144143888+F1eshka@users.noreply.github.com>
2024-09-12 16:41:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HW.Arrays
+{
+
+    class Program
+    {
+//        static void Main()
+//        {
+//            int N = 5;
+//            int M = 6;
+
+//            int[,] array = new int[N, M];
+//            int counter = 1;
+
+//            for (int i = 0; i < N; i++)
+//            {
+//                for (int j = 0; j < M; j++)
+//                {
+//                    array[i, j] = counter++;
+//                }
+//            }
+
+//            for (int i = 0; i < N; i++)
+//            {
+//                for (int j = 0; j < M; j++)
+//                {
+//                    Console.Write(array[i, j] + " ");
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            int[] arr = { 1, 11, 21, 1211, 111221, 312211, 13112221 };
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == num)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
+
+
